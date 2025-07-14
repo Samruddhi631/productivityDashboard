25ea7c76e9a5850de65a88956a7d9a46d25a8d4f
samruddhi <samruddhisuryawanshi139@gmail.com>
2025-07-14 22:11:11 +0530

    Workong with ToDo list

--- script.js
+++ script.js
@@ -14,8 +14,25 @@ function openFeature(){
     })
 }
 // openFeature()
-let form=document.querySelector('.addTask form')
+let form=document.querySelector('.addTask form');
+let taskInput=document.querySelector('.addTask form #task-input');
+let taskDetailInput=document.querySelector('.addTask form textarea');
+let taskCheckBox=document.querySelector('.addTask form #check');
+let allTask=[
+    {
+        task:'Mandir Jao',
+        details:'Hanuman jee vale',
+        imp:true
+    },
+    {
+        task:'Recording karo',
+        details:'cohort ke liye',
+        imp:true
+    },
+]
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
-    console.log("hello")
+    console.log(taskInput.value);
+    console.log(taskDetailInput.value);
+    console.log(taskCheckBox.checked)
 })
